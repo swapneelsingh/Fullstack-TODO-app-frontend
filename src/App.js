e8432209479bbe534f9776fa5909b36729404c23
Swapneel singh <122475751+swapneelsingh@users.noreply.github.com>
2025-05-24 01:54:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import TodoComp from "./components/todoComp";
-import { addToDo, getAllToDo, updateTodo, deleteTodo} from "./utils/handleAPI";
+import { addToDo, getAllToDo, updateTodo, deleteTodo } from "./utils/handleAPI";
 
 function App() {
   const [toDo, setToDo] = useState([]);
@@ -21,30 +21,39 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Todo App</h1>
+        <h1 className="title">
+          <span className="hindi">कार्य</span>
+          <span className="english">KRAM</span>
+        </h1>
         <div className="top">
-          <input 
-          type="text" 
-          placeholder="Add TODOs"
-          value={text}
-          onChange={(e) => setText(e.target.value)} />
-          <div 
-          className="add" 
-          onClick={isUpdating ? 
-            ()=> updateTodo(todoID, text, setText, setToDo, setIsUpdating) 
-          : () => addToDo(text, setText, setToDo)}>
-            {isUpdating ? "Update" : "Add"}</div>
+          <input
+            type="text"
+            placeholder=" ADD TODOs "
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <div
+            className="add"
+            onClick={
+              isUpdating
+                ? () =>
+                    updateTodo(todoID, text, setText, setToDo, setIsUpdating)
+                : () => addToDo(text, setText, setToDo)
+            }
+          >
+            {isUpdating ? "UPDATE" : "ADD"}
+          </div>
         </div>
 
         <div className="list">
-          {toDo.map((item) => 
-            <TodoComp 
-            key={item._id} 
-            text={item.text}
-            updateMode={() => updateMode(item._id, item.text)} 
-            deleteTodo={() => deleteTodo(item._id, setToDo)}/>
-           )}
-
+          {toDo.map((item) => (
+            <TodoComp
+              key={item._id}
+              text={item.text}
+              updateMode={() => updateMode(item._id, item.text)}
+              deleteTodo={() => deleteTodo(item._id, setToDo)}
+            />
+          ))}
         </div>
       </div>
     </div>
